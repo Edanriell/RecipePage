fa2cb4637ab148ed9a5ba84bb6eefdf7fe84ed15
Edanriell <Edanriell@gmail.com>
2024-03-22 14:24:09 +0200

    added function get recipes query

--- client/src/entities/recipe/api/api.ts
+++ client/src/entities/recipe/api/api.ts
@@ -1,3 +1,26 @@
-export const getRecipesQuery = () => {
-	return null;
+export const getRecipesQuery = async () => {
+	try {
+		const response = await fetch("path", {
+			method: "Get"
+		});
+
+		if (response.status === 404) {
+			throw new Error("Recipes not found");
+		}
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+
+		const contentType = response.headers.get("content-type");
+		if (!contentType || !contentType.includes("application/json")) {
+			throw new Error("Response is not in JSON format");
+		}
+
+		return response.json();
+	} catch (error) {
+		console.error("Error fetching data:", error);
+	}
 };
+
+// apiUrl = ""; env
